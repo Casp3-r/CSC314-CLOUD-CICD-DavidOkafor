fea452f1367c528e268c4da44b6a0b6a9d6bf5d7
Casp3-r <135520045+Casp3-r@users.noreply.github.com>
2024-05-29 14:28:43 -0700

    new one

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send(' THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATED WITH AWS, THIS PAGE INDICATES SUCCESSS. ROLAND EMMANUEL TERRY VUG/CSC/21/5256'));
+app.get('/', (req, res) => res.send(' THIS IS CSC 314 CLOUD COMPUTING DEVOPS; CI-CD PIPELINE CREATEDWITH AWS. THIS PAGE INDICATES SUCCESS.YOURNAME: DAVID OKAFORSMATRIC NUMBER: VUG/CSC/21/5191'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
